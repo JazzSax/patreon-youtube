e5b5f274ca5cc1c525fef2aa162370bb6130f37d
John Bernard Sahagun <jhnbrnrdshgn@gmail.com>
2025-08-17 12:15:29 +0800

    added messages

---
+++ action/sendMessage.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { client } from "@/lib/schematic";
+import { adminClient } from "@/sanity/lib/adminClient";
+
+export async function sendMessage(message: string) {
+  const user = await currentUser();
+  if (!user) {
+    return { success: false, error: "User not authenticated" };
+  }
+  try {
+    const entitlements = await client.entitlements.getFeatureUsageByCompany({
+      keys: {
+        id: user.id,
+      },
+    });
+
+    const feature = entitlements.data.features.find(
+      (entitlement) => entitlement.feature?.eventSubtype === "send-dm"
+    );
+    const dmUsage = feature?.usage || 0;
+    const dmAllocation = feature?.allocation || 0;
+    if (dmUsage >= dmAllocation) {
+      return { success: false, error: "Feature usage exceeded" };
+    }
+    if (!feature) {
+      return { success: false, error: "Feature not found" };
+    }
+
+    const newMessage = await adminClient.create({
+      _type: "message",
+      senderName:
+        user.fullName || user.emailAddresses[0]?.emailAddress || "Unknown",
+      senderEmail: user.emailAddresses[0]?.emailAddress,
+      messageBody: message,
+    });
+
+    await client.track({
+      event: "send-dm",
+      company: {
+        id: user.id,
+      },
+      user: {
+        id: user.id,
+      },
+    });
+
+    const updatedDmUsage = dmUsage + 1;
+    return {
+      success: true,
+      message: newMessage,
+      usage: updatedDmUsage,
+      allocation: dmAllocation,
+    };
+  } catch (error) {
+    console.error("Error sending message:", error);
+    return { success: false, error: "Failed to send message" };
+  }
+  return { success: true, message: "Message sent successfully" };
+}
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,43 @@
-import type {StructureResolver} from 'sanity/structure'
-
+import type { StructureResolver } from "sanity/structure";
+import { MailPlus, MailCheck, MailOpen } from "lucide-react";
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Content")
+    .items([
+      ...S.documentTypeListItems().filter(
+        (listItem) => listItem.getId() !== "message"
+      ),
+
+      S.listItem()
+        .title("VIP Messages")
+        .icon(MailPlus)
+        .child(
+          S.list()
+            .title("VIP Messages")
+            .items([
+              S.listItem()
+                .title("New Messages")
+                .icon(MailCheck)
+                .child(
+                  S.documentList()
+                    .title("New Messages")
+                    .filter('_type == "message" && isRead != true')
+                    .defaultOrdering([
+                      { field: "_createdAt", direction: "desc" },
+                    ])
+                ),
+              S.listItem()
+                .title("Read Messages")
+                .icon(MailOpen)
+                .child(
+                  S.documentList()
+                    .title("Read Messages")
+                    .filter('_type == "message" && isRead == true')
+                    .defaultOrdering([
+                      { field: "_createdAt", direction: "desc" },
+                    ])
+                ),
+            ])
+        ),
+    ]);
